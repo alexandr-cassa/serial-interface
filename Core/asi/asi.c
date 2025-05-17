adaaf281a198503b13af3553337773ef6a927f49
alexandr <alexandr.cassa@gmail.com>
2025-05-17 12:55:58 +0300

    Impl without testing

--- Core/asi/asi.c
+++ Core/asi/asi.c
@@ -8,31 +8,35 @@
 
 #include "asi.h"
 #include "asi_typedefs.h"
+#include "asi_rx/asi_rx.h"
+#include "asi_tx/asi_tx.h"
 
 extern void ASI_init() {
 
 }
 
 extern void ASI_write(uint8_t* data, uint16_t size) {
-
+	TX_send(data, size);
 }
 
 extern void ASI_read(uint8_t* data, uint16_t size) {
-
+	RX_read(data, size);
 }
 
 extern void ASI_transmitterTimerCallback(void) {
-
+	TX_timerCompleteCallback();
 }
 
 extern void ASI_receiverTimerCallback(void) {
-
+	RX_runStopStateOnTimerFullPeriodEvent();
+	RX_runReceivingStateOnTimerFullPeriodEvent();
+	RX_runWaitStateOnTimerHalfPeriodEvent();
 }
 
 extern void ASI_receiverFallingEdgeDataPinCallback(void) {
-
+	RX_runSyncFirstStepOnFallingEdgeEvent();
 }
 
 extern void ASI_receiverRisingEdgeDataPinCallback(void) {
-
+	RX_runSyncSecondStateOnRisingEdgeEvent();
 }
